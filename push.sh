40b5cf5eae87baae5bcb4a1e3b1f41f4febbc308
bhr0554 <bhr5004@gmail.com>
2020-07-25 01:30:01 +0900

    push2

--- push.sh
+++ push.sh
@@ -1,16 +1,18 @@
 echo "###########################"
-echo "Start of push"
+echo "#######Start of push#######"
 
 cd public/
 git add *
 git commit -m "$1"
 git push origin master
 
+echo "###########################"
+
 cd ../
 git add *
 git commit -m "$2"
 git push origin master
 
-echo "End of push"
+echo "########End of push########"
 echo "###########################"
 
