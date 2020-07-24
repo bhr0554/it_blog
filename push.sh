cb457c43df278ad993554404e474ba07cf7f205c
bhr0554 <bhr5004@gmail.com>
2020-07-25 01:27:20 +0900

    push2

--- push.sh
+++ push.sh
@@ -3,12 +3,12 @@ echo "Start of push"
 
 cd public/
 git add *
-git commit -m "post sub"
+git commit -m "$1"
 git push origin master
 
 cd ../
 git add *
-git commit -m "post main"
+git commit -m "$2"
 git push origin master
 
 echo "End of push"
